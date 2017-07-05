48a20e1140d18269364e15b83b5ca7b04fb541ba
Will Gant <w_gant@yahoo.co.uk>
2017-07-05 12:34:51 +0000

    Comparer returns list of matching names, updates to node 8

--- src/Comparer.js
+++ src/Comparer.js
@@ -1,5 +1,15 @@
 function Comparer() {
-    
+    this.matchingNames = []
+};
+
+Comparer.prototype.compare = function(namesList1, namesList2) {
+    var matchingNames = this.matchingNames
+    namesList1.forEach(function(name) {
+       if(namesList2.includes(name)) {
+            matchingNames.push(name)
+       } 
+    });
+    return matchingNames
 };
 
 module.exports = Comparer;
